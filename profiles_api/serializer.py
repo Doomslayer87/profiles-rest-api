cd2d16a4487d5afe04cecf98068047eb86106895
Riccardo <riccardo.marchi.rads@gmail.com>
2025-04-10 12:27:38 +0200

    spostato su flask

--- profiles_api/serializer.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-class HelloSerializer(serializers.Serializer):
-    """Serializers a name field for testing an api view. accepts a name input to add to api view as a POST request. funzionano come form da compilare. forniscono una validazione dei dati. nel caso qui sotto controlla che il dato non superi i 10 caratteri"""
-    name = serializers.CharField(max_length=10)
-
